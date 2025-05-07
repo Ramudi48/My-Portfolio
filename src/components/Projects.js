cd5f6325d33fa18dcd2207a39983c5dd1ae3e34e
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-05-07 17:05:39 +0530

    Add Projects component with a list of projects and descriptions

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <p>Here are some of my works:</p>
+      <ul>
+        <li>Project 1 - Description</li>
+        <li>Project 2 - Description</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;

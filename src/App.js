b35b606fb8eb846064b228baad804110cda8dc0d
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-05-17 00:00:36 +0530

    Refactor Projects component to display project details and add styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -10,18 +10,14 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </>
   );
 }
 

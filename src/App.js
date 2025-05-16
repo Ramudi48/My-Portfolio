cdf2b85fc2268f8cd23c911cb0103079ac433119
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-05-16 20:59:33 +0530

    Fix rendering of Skills route in App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={ />} />
+          <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>

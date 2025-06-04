0c8cb5be79bb2b206dd7a687f23e7f8f2cd6c730
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-06-04 23:31:51 +0530

    Fix Home.js; correct typo in "amnd" to "and"

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
 
         <p>
           Passionate and driven undergraduate student with a strong foundation in computer science and programming.
-          Equipped with problem-solving skills and a desire to continuously learn and grow. Seeking opportunities to apply
+          Equipped with problem-solving skills and a desire to continuously learn amnd grow. Seeking opportunities to apply
           technical knowledge to real-world challenges and collaborate on impactful projects.
         </p>
 

20bf9498e6984fb809d921a685ed23b73ad3b1ab
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-06-05 22:23:45 +0530

    Fix Contact.js; correct typo in button text from "Sendmm Message" to "Send Message"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => {
             required
           ></textarea>
 
-          <button type="submit">Send Message</button>
+          <button type="submit">Sendmm Message</button>
         </form>
       </div>
     </section>

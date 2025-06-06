5480f8ede3f07efb9ad9bc3be7e5e90a7ce31ea2
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-06-06 21:06:08 +0530

    Fix Navbar.js; correct typo in button text from "Lightm Mode" to "Light Mode"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = ({ setActiveSection }) => {
             className="dark-mode-btn"
             onClick={() => setIsDarkMode(!isDarkMode)}
           >
-            {isDarkMode ? "Lightm Mode💡" : "Dark Mode🌙"}
+            {isDarkMode ? "Light Mode💡" : "Dark Mode🌙"}
           </button>
         </li>
       </ul>

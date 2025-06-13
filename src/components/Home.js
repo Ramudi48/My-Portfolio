11ea61a5e5cefb0317c617634e565d4a8ceeba9a
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-06-13 20:48:07 +0530

    Fix Home.js; correct typo in alt attribute from "Ramudi Munamsinghe" to "Ramudi Munamsinghe"

--- src/components/Home.js
+++ src/components/Home.js
@@ -49,7 +49,7 @@ const Home = () => {
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1 }}
       >
-        <img src="/profile.jpg" alt="Ramudi Munasinghe" />
+        <img src="/profile.jpg" alt="Ramudi Munamsinghe" />
       </motion.div>
     </section>
   );
